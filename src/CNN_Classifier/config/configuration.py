420e7ff659cb504f2d191f0c55a8d3ae8ff08b71
blahblahblah1920 <pranavabishaimoses@gmail.com>
2023-12-30 12:41:59 -0600

    call back added

--- src/CNN_Classifier/config/configuration.py
+++ src/CNN_Classifier/config/configuration.py
@@ -1,6 +1,7 @@
 from CNN_Classifier.constants import *
-from CNN_Classifier.entity.config_entity import DataIngestionCongig, PrepareBaseModelConfig
+from CNN_Classifier.entity.config_entity import DataIngestionCongig, PrepareBaseModelConfig, PrepareCallbacksConfig
 from CNN_Classifier.utilities.common import read_yaml, create_directories
+import os
 
 class ConfigurationManager:
     def __init__(
@@ -42,3 +43,19 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
